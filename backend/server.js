b30e6a23bbda36437dca62af1fa221b3402e5e6f
krish-gupta53 <krish.gupta8054@gmail.com>
2025-06-21 20:02:14 +0530

    pushing

--- backend/server.js
+++ backend/server.js
@@ -38,4 +38,4 @@ app.get("/", (req, res) => {
     res.send("API Working")
 });
 
-app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
+app.listen(port,'0.0.0.0', () => console.log(`Server started on http://localhost:${port}`))
